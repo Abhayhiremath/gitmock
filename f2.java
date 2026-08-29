7360b1b5704d9cfc29fc60e2d965036654387e0e
Abhayhiremath <abhayhiremath00@gmail.com>
2026-08-29 16:21:10 +0530

    adding f2.java in main branch

--- f2.java
+++ f2.java
@@ -1,1 +1,28 @@
-hii dhee coding lab
+public class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println("Addition: " + c.add(20, 10));
+        System.out.println("Subtraction: " + c.subtract(20, 10));
+        System.out.println("Multiplication: " + c.multiply(20, 10));
+        System.out.println("Division: " + c.divide(20, 10));
+    }
+}
