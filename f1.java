ef744bcda870cc1b57af1cebbd153466eb93d14e
Abhayhiremath <abhayhiremath00@gmail.com>
2026-08-29 16:21:07 +0530

    adding f1.java in main branch

--- f1.java
+++ f1.java
@@ -1,1 +1,23 @@
-hi this is a mock test
+public class Student {
+
+    String name;
+    int age;
+    double marks;
+
+    void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        s.name = "Abhay";
+        s.age = 21;
+        s.marks = 85.5;
+
+        s.displayDetails();
+    }
+}
